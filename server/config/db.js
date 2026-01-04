e7323c54fa40723f9331ca3234d5fc4911dcccca
chaitanya-kumar-2004 <chaitanyakumaryerramalla@gmail.com>
2026-01-04 20:35:24 +0530

    database

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,18 @@
 // db.js
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',           // <-- CHANGED
-    password: 'Mintu@2004',   // <-- CHANGED
-    database: 'handicraft_store',
+  host: "${{RAILWAY_PRIVATE_DOMAIN}}",
+  user: "root",
+  password:"${{MYSQL_ROOT_PASSWORD}}",
+  database: "railway",
+  port: "3306",
+  waitForConnections: true,
+  connectionLimit: 10,
 });
+
 module.exports = pool.promise();
+
+
+
+
